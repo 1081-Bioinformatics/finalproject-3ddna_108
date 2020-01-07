2d74804ea0b839e691c0766d3367c02638c5e6bf
whuang022nccu <whuang022.nccu@gmail.com>
2020-01-07 08:27:41 +0800

    add run shell script

---
+++ code/run_analysis/run_AaegL2_script.sh
@@ -0,0 +1,2 @@
+chmod +x run-asm-pipeline.sh
+nohup ./run-asm-pipeline.sh AaegL2.fasta GSE95797_AaegL2.mnd.txt > run_AaegL2.log 2>&1 &
--- code/run_analysis/run_Human_script.sh
+++ code/run_analysis/run_Human_script.sh
@@ -1,2 +1,2 @@
 chmod +x run-asm-pipeline.sh
-nohup ./run-asm-pipeline.sh GSE95797_Hs1.fasta GSE95797_Hs1.mnd.txt > run.log 2>&1 &
+nohup ./run-asm-pipeline.sh GSE95797_Hs1.fasta GSE95797_Hs1.mnd.txt > run_GSE95797_Hs1.log 2>&1 &
